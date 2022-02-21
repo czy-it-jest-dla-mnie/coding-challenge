3073844b8d82241091bc1207e19f991ace990e89
krzysztof-jastrzebski <kjj89@o2.pl>
2022-02-21 16:39:10 +0100

    Zmieniłem pierwsze litery nazw zmiennych z wielkich na małe. Dodałem warunek równości w zadaniu 1.3

--- Zadanie1.js
+++ Zadanie1.js
@@ -9,19 +9,21 @@ console.log(aldi.nazwa + ": " + aldi.produkty.length);
 
 // zad 1.3: który sklep ma więcej miejsc parkingowych, lidl czy aldi
 if(lidl.iloscMiejscParkingowych > aldi.iloscMiejscParkingowych) {
-      console.log("lidl ma więcej miejsc parkingowych");
+ console.log("lidl ma więcej miejsc parkingowych");
+} else if(lidl.iloscMiejscParkingowych == aldi.iloscMiejscParkingowych) {
+ console.log("lidl i aldi mają tyle samo miejsc parkingowych")
 } else {
-      console.log("aldi ma więcej miejsc parkingowych");
+ console.log("aldi ma więcej miejsc parkingowych");
 }
 
 // zad 1.4: ile jest wszystkich miejsc parkingowych w sumie przy biedrze, aldim i lidlu
-var LidlMiejsca = lidl.iloscMiejscParkingowych;
-var BiedronkaMiejsca = biedronka.iloscMiejscParkingowych;
-var AldiMiejsca = aldi.iloscMiejscParkingowych;
-var WszystkieMiejsca = LidlMiejsca + BiedronkaMiejsca + AldiMiejsca;
-console.log("wszystkie miejsca parkingowe" + ": " + WszystkieMiejsca);
+var lidlMiejsca = lidl.iloscMiejscParkingowych;
+var biedronkaMiejsca = biedronka.iloscMiejscParkingowych;
+var aldiMiejsca = aldi.iloscMiejscParkingowych;
+var wszystkieMiejsca = lidlMiejsca + biedronkaMiejsca + aldiMiejsca;
+console.log("wszystkie miejsca parkingowe" + ": " + wszystkieMiejsca);
 
 // zad 1.5: jaka jest srednia liczba miejsc parkingowych przy biedrze aldim i lidlu
-var SredniaMiejsc = WszystkieMiejsca / sklepy.length;
-console.log("średnia ilość miejsc parkingowych:" + ": " + SredniaMiejsc);
+var sredniaMiejsc = wszystkieMiejsca / sklepy.length;
+console.log("średnia ilość miejsc parkingowych:" + ": " + sredniaMiejsc);
 
