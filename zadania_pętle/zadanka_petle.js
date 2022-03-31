1f8ba80193648147c3958811e97cff94596f45da
Filip Paluch <filip.paluch@o2.pl>
2022-03-31 20:53:09 +0200

    Add files via upload

---
+++ zadania_pętle/zadanka_petle.js
@@ -0,0 +1,104 @@
+var users = [{
+	name: 'Filip',
+  surname: 'Paluch',
+  age: 30,
+  address: {
+  	country: 'Italy',
+  },
+  emails: ['filip@gmail.com', 'paluch@gmail.com'],
+  expenses: [
+  	{
+  		 title: 'Maslo',
+       price: 5.99
+  	},
+    {
+  		 title: 'Chleb',
+       price: 4.80
+  	},
+    {
+  		 title: 'Szynka',
+       price: 18.50
+  	}
+  ]
+},
+{
+  name: 'Damian',
+  surname: 'Krychowski',
+  age: 31,
+  address: {
+  	country: 'Poland'
+  },
+  emails: ['damian@gmail.com', 'krychowski@o2.pl'],
+  expenses: [
+  	{
+  		 title: 'Woda',
+       price: 15.99
+  	},
+    {
+  		 title: 'Mieso',
+       price: 43.00
+  	}
+  ]
+},
+
+{
+  name: 'Jan',
+  surname: 'Kowalski',
+  age: 17,
+  address: {
+  	country: 'Poland'
+  },
+  emails: [],
+  expenses: [
+  	{
+  		 title: 'Kawior',
+       price: 150.99
+  	},
+    {
+  		 title: 'Mieso',
+       price: 67.40
+  	}
+  ]
+},
+
+{
+  name: 'Jan',
+  surname: 'Nowak',
+  age: 25,
+  address: {
+  	country: 'Spain'
+  },
+  emails: ['jan.nowak@o2.pl'],
+  expenses: [
+  	{
+  		 title: 'Pieczywo',
+       price: 6.99
+  	},
+    {
+  		 title: 'Schab',
+       price: 27.40
+  	}
+  ]
+}]
+
+// 1. Wypisz w pętli 'for of' imiona wszystkich użytkowników
+
+// 2. Wypisz w pętli 'for' imiona wszystkich użytkowników
+
+// 3. Wypisz w pętli (dowolnej) imię i nazwisko wszystkich użytkownikow którzy mieszkają w Polsce
+// Podpowiedz: użyj warunku 'if'
+
+// 4. Zsumuj wiek wszystkich użytkowników w pętli (dowolnej) i wypisz ostateczny wynik - wypisać powinnien się tylko jeden raz wynik końcowy.
+
+// 5. Policz za pomocą pętli (dowolnej) średnią wieku wszystkich użytkowników
+
+// 6. Wypisz za pomocą pętli 'for of' wszystkie emaile wszystkich użytkowników
+
+// 7. Wypisz za pomocą pętli 'while' wszystkie emaile które są na 'gmail' wszystkich użytkowników
+
+// 8. Podsumuj wszystkie wydatki wszystkich użytkowników i wypisz wartość końcową
+
+// 9. Podsumuj wydatki na mięso wszystkich użytkowników
+
+// 10. Wyznacz i wypisz użytkownika który wydal najwięcej oraz wypisz tą kwotę
+
