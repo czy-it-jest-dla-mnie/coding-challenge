4a0355d6ee3f00af8631dc7fc6ad555f4411bcf1
Bartosz-Jasiulewicz <bartosz.jasulewicz96@gmail.com>
2022-04-08 16:59:09 +0200

    zadania od 1.1 do 2.1

--- challenge-1-2022.js
+++ challenge-1-2022.js
@@ -87,19 +87,68 @@ var sklepy = [lidl, biedronka, aldi];
 
 // zad 1.1: wypisz nazwy wszystkich sklepów
 
+for (var i=0; i < sklepy.length; i++)	console.log(sklepy[i].nazwa);
+
 // zad 1.2: wypisz ile produktów jest w każdym sklepie w formacie {nazwa sklepu}: {ilosc produktow}
+
+for (var i=0; i < sklepy.length; i++)	console.log(sklepy[i].nazwa + ": " + sklepy[i].produkty.length);
 
 // zad 1.3: który sklep ma więcej miejsc parkingowych, lidl czy aldi
 
+if (lidl.iloscMiejscParkingowych >= biedronka.iloscMiejscParkingowych & lidl.iloscMiejscParkingowych >= aldi.iloscMiejscParkingowych) {
+  if (lidl.iloscMiejscParkingowych == biedronka.iloscMiejscParkingowych & lidl.iloscMiejscParkingowych == aldi.iloscMiejscParkingowych) {
+    console.log("We wszystkich sklepach jest tyle samo miejsc parkingowych");
+  }
+  else if (lidl.iloscMiejscParkingowych == biedronka.iloscMiejscParkingowych) {
+    console.log("najwięcej miejsc parkingowych jest w lidlu i biedronce");
+  } else if (lidl.iloscMiejscParkingowych == aldi.iloscMiejscParkingowych) {
+    console.log("najwięcej miejsc parkingowych jest w lidlu i aldim");
+  } else {
+  console.log("najwięcej miejsc parkingowych jest w lidlu");
+  }
+} else if (biedronka.iloscMiejscParkingowych >= lidl.iloscMiejscParkingowych & biedronka.iloscMiejscParkingowych >= aldi.iloscMiejscParkingowych) {
+  if (biedronka.iloscMiejscParkingowych == lidl.iloscMiejscParkingowych) {
+    console.log("najwięcej miejsc parkingowych jest w biedronce i lidlu");
+  } else if (biedronka.iloscMiejscParkingowych == aldi.iloscMiejscParkingowych) {
+    console.log("najwięcej miejsc parkingowych jest w biedronce i aldim");
+  } else {
+	console.log("najwięcej miejsc parkingowych jest w biedronce");
+  }
+} else if (aldi.iloscMiejscParkingowych >= biedronka.iloscMiejscParkingowych & aldi.iloscMiejscParkingowych >= lidl.iloscMiejscParkingowych) {
+  if (aldi.iloscMiejscParkingowych == biedronka.iloscMiejscParkingowych) {
+    console.log("najwięcej miejsc parkingowych jest w aldim i biedronce");
+  } else if (aldi.iloscMiejscParkingowych == lidl.iloscMiejscParkingowych) {
+    console.log("najwięcej miejsc parkingowych jest w aldim i lidlu");
+  } else {
+    console.log("najwięcej miejsc parkingowych jest w aldim");
+  } 
+}
+
 // zad 1.4: ile jest wszystkich miejsc parkingowych w sumie przy biedrze, aldim i lidlu
 
+for (sum = 0, i = 0; i < sklepy.length; i++) {
+	sum= sum+sklepy[i].iloscMiejscParkingowych;
+}
+console.log ("We wszystkich sklepach w sumie jest " + sum + " miejsc parkingowych");
+
 // zad 1.5: jaka jest srednia liczba miejsc parkingowych przy biedrze aldim i lidlu
+
+for (avg = 0, i = 0; i < sklepy.length; i++) {
+	avg= avg+sklepy[i].iloscMiejscParkingowych;
+}
+console.log ("średnia ilości miejsc parkingowych we wszystkich sklepach wynosi " + avg/sklepy.length);
 
 // Dla trochę bardziej wtajemniczonych
 
 // Założenia - operuj na kolekcji sklepy - twój kod powinien zadziałać także gdybyśmy dorzucili nagle więcej sklepów
 
 // zad 2.1: wypisz wszystkie produkty w formacie: {sklep}:{produkt}:{cena}
+
+for (i = 0; i<sklepy.length; i++) {
+	for (j = 0; j<sklepy[i].produkty.length; j++) {
+  console.log(sklepy[i].nazwa + ": " + sklepy[i].produkty[j].nazwa + ": " + sklepy[i].produkty[j].cena);
+  }
+}
 
 // zad 2.2: wypisz nazwy wszystkich produktów, tak, żeby każda nazwa wystąpiła maksymalnie tylko 1 raz
 
